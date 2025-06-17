6045b84369a1f11b4e390ded3a720749c27ed970
Rafaela Simili <rafaela.simili.lima@gmail.com>
2025-06-17 09:50:32 -0300

    Trabalhando com variáveis

---
+++ backend/lojicaProgramacao/01_variaveis.py
@@ -0,0 +1,32 @@
+nome = "Rafaela"
+idade = 17
+altura = 1,60
+tem_carteira = True
+
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Tem carteira:", tem_carteira)
+
+
+
+
+nome = "Rafaela"
+sobrenome = "Simili Lima"
+idade = 17
+peso = 56
+altura = 1.60
+miopia = 9.25
+usa_oculos = True
+tem_cabelo_longo = True
+
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Peso:", peso)
+print("Altura:", altura)
+print("Miopia:", miopia)
+print("Usa oculos:", usa_oculos)
+print("Tem cabelo longo:", tem_cabelo_longo)
+
+
